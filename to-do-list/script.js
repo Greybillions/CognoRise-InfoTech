3865e906f560ebdeb9ee1f4206d2c8bcd935b63a
Graham Boyle <olayile1999@gmail.com>
2024-04-14 17:28:58 +0100

    updated

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -13,13 +13,11 @@ const time = () => {
   const data = new Date();
   let h = data.getHours();
   let m = data.getMinutes();
-  let s = data.getSeconds();
 
   if (h < 10) h = '0' + h;
   if (m < 10) m = '0' + m;
-  if (s < 10) s = '0' + s;
 
-  document.querySelector('#hour').innerHTML = `${h}:${m}:${s}`;
+  document.querySelector('#hour').innerHTML = `${h}:${m}`;
   setTimeout('time()', 500);
 };
 
