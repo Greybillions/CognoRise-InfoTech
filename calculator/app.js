c7d7f698875d64e51e21452fc27cb7ecd9af130a
Graham Boyle <olayile1999@gmail.com>
2024-04-18 13:12:14 +0100

    updated

--- calculator/app.js
+++ calculator/app.js
@@ -1,24 +1,37 @@
 const keys = document.querySelectorAll('.key');
-const displayInput = document.querySelectorAll('.display .input');
-const displayOutput = document.querySelectorAll('.display .output');
+const displayInput = document.querySelector('.display .input');
+const displayOutput = document.querySelector('.display .output');
 
 let input = '';
 
 for (let key of keys) {
-  const value = key.dataset.key;
+  let value = key.dataset.key;
   key.addEventListener('click', () => {
     if (value === 'clear') {
       input = '';
       displayInput.innerHTML = '';
       displayOutput.innerHTML = '';
-    } else if ((value = 'backspace')) {
+    } else if (value === 'backspace') {
       input = input.slice(0, -1);
       displayInput.innerHTML = input;
-    } else if (value == '=') {
+    } else if (value === '=') {
       let result = eval(input);
       displayOutput.innerHTML = result;
-    } else if (value == 'brackets'){
-        if(input)
+    } else if (value === 'brackets') {
+      let openingBrackets = input.split('(').length - 1;
+      let closingBrackets = input.split(')').length - 1;
+
+      if (openingBrackets > closingBrackets) {
+        input += ')';
+      } else {
+        input += '(';
+      }
+
+      displayInput.innerHTML = input;
+    } else {
+      input += value;
+      displayInput.innerHTML = input;
     }
+    console.log(value);
   });
 }
