2b00e8c50a0e4f62962c8dd2ce949c4b6d2ac269
Graham Boyle <olayile1999@gmail.com>
2024-04-19 09:03:04 +0100

    updated

--- calculator/app.js
+++ calculator/app.js
@@ -15,8 +15,8 @@ for (let key of keys) {
       input = input.slice(0, -1);
       displayInput.innerHTML = clearInput(input);
     } else if (value === '=') {
-      let result = eval(input);
-      displayOutput.innerHTML = result;
+      let result = eval(prepareInput(input));
+      displayOutput.innerHTML = clearOutput(result);
     } else if (value === 'brackets') {
       let openingBrackets = input.split('(').length - 1;
       let closingBrackets = input.split(')').length - 1;
@@ -29,8 +29,10 @@ for (let key of keys) {
 
       displayInput.innerHTML = clearInput(input);
     } else {
-      input += value;
-      displayInput.innerHTML = clearInput(input);
+      if (validateInput(value)) {
+        input += value;
+        displayInput.innerHTML = clearInput(input);
+      }
     }
   });
 }
@@ -47,9 +49,9 @@ const clearInput = (input) => {
     } else if (inputArray[i] === '-') {
       inputArray[i] = `<span> <i class="operator fas fa-minus"></i> </span>`;
     } else if (inputArray[i] === '+') {
-      inputArray[i] = `<span> <i class="percent fas fa-plus"></i> </span>`;
+      inputArray[i] = `<span> <i class="operator fas fa-plus"></i> </span>`;
     } else if (inputArray[i] === '%') {
-      inputArray[i] = `<span> <i class="operator fas fa-percent"></i> </span>`;
+      inputArray[i] = `<span> <i class="percent fas fa-percent"></i> </span>`;
     } else if (inputArray[i] === '(') {
       inputArray[i] = `<span class="brackets">(</span>`;
     } else if (inputArray[i] === ')') {
@@ -60,4 +62,55 @@ const clearInput = (input) => {
   return inputArray.join('');
 };
 
-const clearOutput = (output) => {};
+const clearOutput = (output) => {
+  let outputString = output.toString();
+  let decimal = outputString.split('.');
+
+  outputString = outputString.split('.')[0];
+
+  let outputArray = outputString.split('');
+
+  if (outputArray.length > 3) {
+    for (let i = outputArray.length - 3; i > 0; i -= 3) {
+      outputArray.splice(i, 0, ',');
+    }
+  }
+
+  if (decimal) {
+    outputArray.push('.');
+    outputArray.push(decimal);
+  }
+
+  return outputArray.join('');
+};
+
+const validateInput = (value) => {
+  let lastInput = input.slice(-1);
+  let operators = ['+', '-', '*', '/', '%'];
+
+  if (value === '.' && lastInput === '.') {
+    return false;
+  }
+
+  if (operators.includes(value)) {
+    if (operators.includes(lastInput)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  return true;
+};
+
+const prepareInput = (input) => {
+  let inputArray = input.split('');
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === '%') {
+      inputArray[i] = '/100';
+    }
+  }
+
+  return inputArray.join('');
+};
